d45da5ae121e50651f2eb0b0bcb932f527123bfb
noveshgoyal <goyalnovesh@gmail.com>
2022-06-07 21:01:26 +0530

    making it unbeatable with minmax algo

--- script.js
+++ script.js
@@ -1,92 +1,145 @@
 var origBoard;
-var human = 'X';
-var ai = 'O';
+var human = "X";
+var ai = "O";
 var winCombos = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-const cells = document.querySelectorAll('.cell');
+const cells = document.querySelectorAll(".cell");
 startGame();
 
-function startGame(){
-    document.querySelector('.endgame').style.display = 'none';
-    origBoard = Array.from(Array(9).keys());
-    for(let i=0; i<cells.length; i++){
-        cells[i].innerText = '';
-        cells[i].style.removeProperty('background-color');
-        cells[i].addEventListener('click', turnClick, false);
-    }
+function startGame() {
+  document.querySelector(".endgame").style.display = "none";
+  origBoard = Array.from(Array(9).keys()); // keys method returns a object that contains the keys for each index in the array;
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerText = "";
+    cells[i].style.removeProperty("background-color");
+    cells[i].addEventListener("click", turnClick, false);
+  }
 }
 
-function turnClick(square){
-   if(typeof origBoard[square.target.id] == 'number'){
+function turnClick(square) {
+  if (typeof origBoard[square.target.id] == "number") {
     turn(square.target.id, human);
-    if(!checkTie()) turn(bestSpot(), ai);
-   }
+    if (!checkWin(origBoard, human) && !checkTie()) turn(bestSpot(), ai);
+  }
 }
 
-function turn(id, player){
-    origBoard[id] = player;
-    document.getElementById(id).innerText = player;
-    let gameWon = checkWin(origBoard, player);
-    if(gameWon) gameOver(gameWon);
+function turn(id, player) {
+  origBoard[id] = player;
+  document.getElementById(id).innerText = player;
+  let gameWon = checkWin(origBoard, player);
+  if (gameWon) gameOver(gameWon);
 }
 
-function checkWin(board, player){
-    let plays = board.reduce((a, e, i) => (e===player)? a.concat(i) : a, []);
-    let gameWon =null;
-    for(let [index, win] of winCombos.entries()){
-        if(win.every(elem => plays.indexOf(elem) > -1)){
-            gameWon = {index, player};
-            break;
-        }
+function checkWin(board, player) {
+  let plays = board.reduce((a, e, i) => (e === player ? a.concat(i) : a), []);
+  let gameWon = null;
+  for (let [index, win] of winCombos.entries()) {
+    if (win.every((elem) => plays.indexOf(elem) > -1)) {
+      gameWon = { index, player };
+      break;
     }
-    return gameWon;
+  }
+  return gameWon;
 }
 
-function gameOver(gameWon){
+function gameOver(gameWon) {
+  // Highlighting the squares which are the part of the winning combinaton
+  for (let index of winCombos[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor =
+      gameWon.player == human ? "blue" : "red";
+  }
 
-    // Highlighting the squares which are the part of the winning combinaton
-    for(let index of winCombos[gameWon.index]){
-        document.getElementById(index).style.backgroundColor = gameWon.player == human ? 'blue' : 'red';
+  // Removing the click event so that the user cannot click any square as the game is over
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener("click", turnClick, false);
+  }
+
+  declareWinner(gameWon.player == human ? "You win!" : "You Lose!");
+}
+
+function declareWinner(who) {
+  document.querySelector(".endgame").style.display = "block";
+  document.querySelector(".text").innerText = who;
+}
+
+function emptySquares() {
+  return origBoard.filter((s) => typeof s == "number");
+}
+
+function bestSpot() {
+  // return emptySquares()[0];
+  return minimax(origBoard, ai).index;
+}
+
+function checkTie() {
+  if (emptySquares().length == 0) {
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].style.backgroundColor = "green";
+      cells[i].removeEventListener("click", turnClick, false);
+    }
+    declareWinner("Tie Game!");
+    return true;
+  }
+  return false;
+}
+
+function minimax(newBoard, player) {
+  let availSpots = emptySquares(newBoard);
+
+  if (checkWin(newBoard, player)) {
+    return { score: -10 };
+  } else if (checkWin(newBoard, ai)) {
+    return { score: 10 };
+  } else if (availSpots.length == 0) {
+    return { score: 0 };
+  }
+
+  var moves = [];
+  for (let i = 0; i < availSpots.length; i++) {
+    var move = {};
+    move.index = newBoard[availSpots[i]];
+    newBoard[availSpots[i]] = player;
+
+    if (player == ai) {
+      let result = minimax(newBoard, human);
+      move.score = result.score;
+    } else {
+      let result = minimax(newBoard, ai);
+      move.score = result.score;
     }
 
-    // Removing the click event so that the user cannot click any square as the game is over
-    for(let i=0; i<cells.length; i++){
-        cells[i].removeEventListener('click', turnClick, false);
+    newBoard[availSpots[i]] = move.index;
+
+    moves.push(move);
+  }
+
+  var bestMove;
+  if (player === ai) {
+    var bestScore = -1000;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
     }
-
-    declareWinner(gameWon.player == human ? "You win!" : "You Lose!");
-}
-
-function declareWinner(who){
-    document.querySelector('.endgame').style.display = 'block';
-    document.querySelector('.endgame .text').innerText = who;
-}
-
-function emptySquares(){
-    return origBoard.filter(s => typeof s == 'number');
-}
-
-function bestSpot(){
-    return emptySquares()[0];
-}
-
-function checkTie(){
-    if(emptySquares().length == 0){
-        for(let i=0; i<cells.length; i++){
-            cells[i].style.backgroundColor = 'green';
-            cells[i].removeEventListener('click', turnClick, false);
-        }
-        declareWinner("Tie Game!");
-        return true;
+  } else {
+    var bestScore = 1000;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
     }
-    return false;
+  }
+
+  return moves[bestMove];
 }
